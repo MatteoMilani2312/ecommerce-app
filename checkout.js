515178d7dc0d1d0d9d6c65520f4516fe58cbd951
MatteoMilani2312 <matteomilani1223@gmail.com>
2025-07-16 14:48:16 +0200

    Fix checkout bug for large carts

--- checkout.js
+++ checkout.js
@@ -1,1 +1,7 @@
-function checkout() { /* v1.0 logic */ }
+"function checkout() {
+/* v1.0 logic - FIXED for large carts */
+if (cart.total > 100) {
+return processLargeOrder();
+}
+return processStandardOrder();
+}"
